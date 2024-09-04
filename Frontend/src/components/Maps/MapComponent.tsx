ca6bac79af1a61f5ad993a9de32ee2287f32849c
EAR1610 <alexreynosoruano741@gmail.com>
2024-09-03 22:44:59 -0600

    Custom Icons in React LeafLet and an options

--- Frontend/src/components/Maps/MapComponent.tsx
+++ Frontend/src/components/Maps/MapComponent.tsx
@@ -8,10 +8,29 @@ import markerIconRetina from 'leaflet/dist/images/marker-icon-2x.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import { MapProps } from '../../helpers/Interfaces';
 
+import firefighterIconImg from '../../assets/firefighterIcon.png';
+import requesterIconImg from '../../assets/EmergencyRequestIcon.png';
+
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon,
   iconRetinaUrl: markerIconRetina,
   shadowUrl: markerShadow,
+});
+
+const firefighterIcon = new L.Icon({
+  iconUrl: firefighterIconImg,
+  iconRetinaUrl: firefighterIconImg,
+  iconSize: [35, 45],
+  iconAnchor: [17, 46],
+  popupAnchor: [0, -46],
+});
+
+const requesterIcon = new L.Icon({
+  iconUrl: requesterIconImg,
+  iconRetinaUrl: requesterIconImg,
+  iconSize: [35, 45],
+  iconAnchor: [17, 46],
+  popupAnchor: [0, -46],
 });
 
 const MapComponent: React.FC<MapProps> = ({ latitude, longitude }) => {
@@ -24,12 +43,29 @@ const MapComponent: React.FC<MapProps> = ({ latitude, longitude }) => {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
+    L.marker(firefighterCoords, { icon: firefighterIcon })
+      .addTo(map)
+      .bindPopup('Ubicación ambulancia')
+      .openPopup();
+
+    L.marker([latitude, longitude], { icon: requesterIcon })
+      .addTo(map)
+      .bindPopup('Ubicación solicitante')
+      .openPopup();
+
     L.Routing.control({
       waypoints: [
         L.latLng(firefighterCoords[0], firefighterCoords[1]),
         L.latLng(latitude, longitude),
       ],
-      routeWhileDragging: true,
+      createMarker: (i, waypoint, n) => {
+        const icon = i === 0 ? firefighterIcon : requesterIcon;
+        const popupText = i === 0 ? 'Ubicación ambulancia' : 'Ubicación solicitante';
+        return L.marker(waypoint.latLng, { icon: icon }).bindPopup(popupText);
+      },
+      draggableWaypoints: false, 
+      addWaypoints: false, 
+      routeWhileDragging: false,
     }).addTo(map);
 
     return () => {
